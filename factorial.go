fcece65a46c2be3bd903cd7207d7c51633e111a3
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2020-02-16 16:42:27 +0700

    Add files via upload

---
+++ factorial.go
@@ -0,0 +1,16 @@
+package main 
+
+import "fmt"
+
+func factorial(number int) int {
+	if number == 0 {
+		return 1
+	}
+	return number * factorial(number -1)
+
+}
+
+func main(){
+	fac := factorial(5)
+	fmt.Println(fac)
+}
